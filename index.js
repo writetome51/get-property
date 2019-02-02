58866bfd334af6bf02590c8738cfa925302418a4
Steve Thompson <writetome51@gmail.com>
2019-02-02 13:52:17 -0700

    added checking for empty string parameter

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var errorIfNotString_1 = require("basic-data-handling/errorIfNotString");
 var errorIfNotObject_1 = require("basic-data-handling/errorIfNotObject");
+var isEmpty_notEmpty_1 = require("basic-data-handling/isEmpty_notEmpty");
 // parameter property is a string that can include dot nation
 // ( i.e,  'property.subproperty.subsubproperty' ) .
 function getProperty(property, obj) {
     errorIfNotString_1.errorIfNotString(property);
+    if (isEmpty_notEmpty_1.isEmpty(property))
+        throw new Error('Input must be string that is not empty.');
     errorIfNotObject_1.errorIfNotObject(obj);
     var properties = property.split('.');
     var numProperties = properties.length;
--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import { errorIfNotString } from 'basic-data-handling/errorIfNotString';
 import { errorIfNotObject } from 'basic-data-handling/errorIfNotObject';
+import { isEmpty } from 'basic-data-handling/isEmpty_notEmpty';
 
 // parameter property is a string that can include dot nation
 // ( i.e,  'property.subproperty.subsubproperty' ) .
 
 export function getProperty(property, obj): any {
 	errorIfNotString(property);
+	if (isEmpty(property)) throw new Error('Input must be string that is not empty.');
 	errorIfNotObject(obj);
+
 	let properties = property.split('.');
 	let numProperties = properties.length;
 
